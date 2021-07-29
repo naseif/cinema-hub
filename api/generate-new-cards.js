c185f28afd0007c58adcfa1cf935feca605ec6c4
naseif <d2mds21@gmail.com>
2021-07-29 10:13:31 +0200

    create new json including the trackid to keep track of the existing deck.

--- api/generate-new-cards.js
+++ api/generate-new-cards.js
@@ -1,16 +1,36 @@
 const fetch = require("node-fetch");
+const fs = require("fs");
 
-async function generateNewDeck(deck) {
+async function generateDeck(decksNumber) {
   try {
     const getNewDeck = await fetch(
-      `http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${deck}`
+      `http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${decksNumber}`
     );
     const res = await getNewDeck.json();
-    return {
-      deckId: res.deck_id,
-      remainingCards: res.remaining,
-    };
-  } catch (error) {
-    throw error;
+    if (fs.existsSync(`${__dirname}/deckData.json`)) {
+      try {
+        fs.unlinkSync(`${__dirname}/deckData.json`);
+      } catch (err) {
+        console.error(`Could not Remove File, ${err.message}`);
+      }
+    }
+    fs.writeFile(
+      `${__dirname}/deckData.json`,
+      `{"deckID": "${res.deck_id}"}`,
+      "UTF-8",
+      (err) => {
+        if (err) throw new Error(`Could not write File!!`);
+      }
+    );
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function replaceOldDeck(decksNumber) {
+  try {
+    const newDeck = await generateDeck(decksNumber);
+  } catch (err) {
+    throw err;
   }
 }
